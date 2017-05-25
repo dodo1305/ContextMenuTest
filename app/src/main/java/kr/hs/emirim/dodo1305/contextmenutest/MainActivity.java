97e71de3bf538a7c40525414ba348986f4bc6d0e
dodo1305 <dodo1305@naver.com>
2017-05-25 19:44:44 +0900

    #4 Event Programming

--- app/src/main/java/kr/hs/emirim/dodo1305/contextmenutest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dodo1305/contextmenutest/MainActivity.java
@@ -1,20 +1,29 @@
 package kr.hs.emirim.dodo1305.contextmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
+    Button but2;
+    ImageView img1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1 = (Button)findViewById(R.id.but1);
         Button but2 = (Button)findViewById(R.id.but2);
+        but2=(Button)findViewById(R.id.but2);
+        linear = (LinearLayout)findViewById(R.id.linear);
+        img1 = (ImageView)findViewById(R.id.img1);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -22,14 +31,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater menuInflater=getMenuInflater();
-        if(v.getId()==R.id.but1){
+        MenuInflater menuInflater = getMenuInflater();
+        if (v.getId() == R.id.but1) {
             menu.setHeaderTitle("배경색 변경");
             //menu.setHeaderIcon(R.drawable.icon1);
-            menuInflater.inflate(R.menu.menu1,menu);
+            menuInflater.inflate(R.menu.menu1, menu);
         }
-        if(v.getId()==R.id.but2){
-            menuInflater.inflate(R.menu.menu2,menu);
+        if (v.getId() == R.id.but2) {
+            menuInflater.inflate(R.menu.menu2, menu);
+    }
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        super.onContextItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.menu1_item_red :
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue :
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink :
+                linear.setBackgroundColor(Color.rgb(255,178,217));
+                return true;
+            case R.id.menu2_item_rotation :
+                img1.setRotation(90);
+                return true;
+            case R.id.menu2_item_size :
+                img1.setScaleX(1.5f);
+                img1.setScaleY(1.5f);
+                return true;
+
         }
+
+        return false;
     }
 }
+
